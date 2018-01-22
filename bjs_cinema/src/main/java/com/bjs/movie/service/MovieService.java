86ff9a7f3d1c5459eab06b089cd0b44abd69a1a5
JO <JO@192.168.35.183>
2018-01-22 15:57:27 +0900

    [지호]180122 15:57 movie 수정

---
+++ bjs_cinema/src/main/java/com/bjs/movie/service/MovieService.java
@@ -0,0 +1,11 @@
+package com.bjs.movie.service;
+
+import java.util.List;
+
+import com.bjs.movie.domain.MovieVO;
+
+public interface MovieService {
+	
+	public void movieInsert(int movie_id)throws Exception;
+	
+}
